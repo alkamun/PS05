af84e0d85ccf6fda02dc882cfc96746d28277338
alkamun <alkamun@yandex.ru>
2025-08-22 21:50:53 +0300

    add - save to csv

--- lamppars/lamppars/settings.py
+++ lamppars/lamppars/settings.py
@@ -85,3 +85,15 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+
+FEEDS = {
+    'lamps.csv' : {
+        'format' : 'csv',
+        'overwrite' : True,
+        'fields' : {
+            'name' : "Наименование",
+            'price' : 'Цена',
+            'url' : 'Ссылка'
+        }
+    }
+}
